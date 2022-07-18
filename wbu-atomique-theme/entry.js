c7b1891c9a727b6587d3bbb22e04fa2453143ac4
noumecha <noumechaivan@gmail.com>
2022-07-18 15:02:32 +0100

    maj : tmc

---
+++ wbu-atomique-theme/entry.js
@@ -0,0 +1,15 @@
+module.exports = {
+    hot_models_overview_section: "./src/js/sections/hot_models_overview_section.js",
+    hot_models_overview_teaser: "./src/js/teasers/hot_models_overview_teaser.js",
+    hot_models_hotlock_security_info: "./src/js/sections/hot_models_hotlock_security_info.js",
+    hot_models_team_teaser: "./src/js/teasers/hot_models_team_teaser.js",
+    hot_models_team_section: "./src/js/sections/hot_models_team_section.js",
+    hot_models_text_btn_block: "./src/js/sections/hot_models_text_btn_block.js",
+    hot_models_lock_smithing: "./src/js/sections/hot_models_lock_smithing.js",
+    hot_models_hotlock_testimony: "./src/js/sections/hot_models_hotlock_testimony.js",
+    hot_models_testimony_teaser_count: "./src/js/sections/hot_models_testimony_teaser_count.js",
+    hot_models_testimony_teaser_text: "./src/js/sections/hot_models_testimony_teaser_text.js",
+    hot_models_hotlock_footer: "./src/js/sections/hot_models_hotlock_footer.js",
+    hot_models_hotlock_back_top: "./src/js/sections/hot_models_hotlock_back_top.js",
+    hot_models_hotlock_menu: "./src/js/sections/menus/hot_models_hotlock_menu.js",
+};
